8bd1da745fd05c1c93ff53808abcec7222339639
Nicklas Knell <n.knell@gastromatic.com>
2024-05-31 11:47:18 +0200

    polymorphic embeddable issue

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,18 +1,84 @@
-import { Entity, MikroORM, PrimaryKey, Property } from "@mikro-orm/postgresql";
+import { Embedded, Enum } from "@mikro-orm/core";
+import {
+  Entity,
+  MikroORM,
+  PrimaryKey,
+  Property,
+  Embeddable,
+} from "@mikro-orm/postgresql";
+import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
+
+@Embeddable()
+class DueDateEmailData {
+  @Property()
+  dueDate: Date;
+
+  @Property()
+  senderEmail: string;
+
+  constructor(args: { dueDate: Date; senderEmail: string }) {
+    this.dueDate = args.dueDate;
+    this.senderEmail = args.senderEmail;
+  }
+}
+
+@Embeddable()
+class EmailData {
+  @Property()
+  senderEmail: string;
+
+  constructor(args: { senderEmail: string }) {
+    this.senderEmail = args.senderEmail;
+  }
+}
+
+enum WrapperType {
+  DUE_DATE = "DUE_DATE",
+  EMAIL = "EMAIL",
+}
+
+@Embeddable({ abstract: true, discriminatorColumn: "type" })
+abstract class PolyEmailDataWrapper {
+  @Enum()
+  type: WrapperType;
+
+  constructor(type: WrapperType) {
+    this.type = type;
+  }
+}
+
+@Embeddable()
+class DueDateEmailDataWrapper extends PolyEmailDataWrapper {
+  @Embedded({ array: true })
+  sentEmails: DueDateEmailData[];
+
+  constructor(args: { sentEmails: DueDateEmailData[] }) {
+    super(WrapperType.DUE_DATE);
+    this.sentEmails = args.sentEmails;
+  }
+}
+
+@Embeddable()
+class EmailDataWrapper extends PolyEmailDataWrapper {
+  @Embedded({ array: true })
+  sentEmails: EmailData[];
+
+  constructor(args: { sentEmails: EmailData[] }) {
+    super(WrapperType.EMAIL);
+    this.sentEmails = args.sentEmails;
+  }
+}
 
 @Entity()
 class User {
   @PrimaryKey()
   id!: number;
 
-  @Property()
-  name: string;
+  @Embedded({ nullable: true, object: true })
+  email: DueDateEmailDataWrapper | EmailDataWrapper;
 
-  @Property({ unique: true })
-  email: string;
-
-  constructor(name: string, email: string) {
-    this.name = name;
+  constructor(id: number, email: DueDateEmailDataWrapper | EmailDataWrapper) {
+    this.id = id;
     this.email = email;
   }
 }
@@ -26,9 +92,16 @@ beforeAll(async () => {
     port: 5432,
     user: "admin",
     password: "admin",
-    entities: [User],
+    entities: [
+      User,
+      DueDateEmailData,
+      EmailData,
+      DueDateEmailDataWrapper,
+      EmailDataWrapper,
+    ],
     debug: ["query", "query-params"],
     allowGlobalContext: true, // only for testing
+    metadataProvider: TsMorphMetadataProvider,
   });
   await orm.schema.refreshDatabase();
 });
@@ -37,17 +110,25 @@ afterAll(async () => {
   await orm.close(true);
 });
 
-test("basic CRUD example", async () => {
-  orm.em.create(User, { name: "Foo", email: "foo" });
-  await orm.em.flush();
+test("embedded", async () => {
+  const user = new User(
+    0,
+    new DueDateEmailDataWrapper({
+      sentEmails: [
+        new DueDateEmailData({
+          dueDate: new Date(),
+          senderEmail: "foo",
+        }),
+      ],
+    })
+  );
+  await orm.em.persistAndFlush(user);
   orm.em.clear();
 
-  const user = await orm.em.findOneOrFail(User, { email: "foo" });
-  expect(user.name).toBe("Foo");
-  user.name = "Bar";
-  orm.em.remove(user);
-  await orm.em.flush();
+  const userQuery = await orm.em.findOneOrFail(User, { id: 0 });
 
-  const count = await orm.em.count(User, { email: "foo" });
-  expect(count).toBe(0);
+  const dueDate = (userQuery.email as DueDateEmailDataWrapper).sentEmails[0]
+    .dueDate;
+  console.log(dueDate);
+  expect(dueDate).toBeDefined();
 });
